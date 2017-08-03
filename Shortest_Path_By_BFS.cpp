dfe8c466fdeaf147c17599c91db91f75bc4be182
Kunal Singh <skunal185@gmail.com>
2017-08-03 22:38:05 +0530

    Add files via upload

---
+++ Shortest_Path_By_BFS.cpp
@@ -0,0 +1,102 @@
+/* Bfs is used to find shortest path when all the edges weights are same */
+
+
+// @ -- Kunal Singh (KSR) --
+
+
+
+
+#include<iostream>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<map>
+#include<cmath>
+#include<cstring>
+#include<queue>
+#include<set>
+
+
+
+using namespace std;
+
+
+
+
+typedef long long int lli;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef map<int,int> mii;
+typedef map<int,char> mic;
+typedef map<char,int> mci;
+typedef map<char,char> mcc;
+typedef set<int> si;
+typedef set<char> sc;
+
+bool visited[100000];
+long long int c[100000],n,m;
+vi nodes[100000];
+
+int BFS(int s)
+{
+
+    for(int i=0;i<=m*n;i++)
+        {
+            c[i]=10000000000;
+            visited[i]=false;
+        }
+
+    queue<int> Q;
+    Q.push(s);
+
+    visited[s]=true;
+    c[s]=0;
+    while(!Q.empty())
+    {
+        int v=Q.front();
+        Q.pop();
+        int b=nodes[v].size();
+
+        for(int i=0;i<b;i++)
+        {
+            //cout<<v<<"->"<<nodes[v][i]<<endl;
+            if(visited[nodes[v][i]]==false)
+            {
+                Q.push(nodes[v][i]);
+                visited[nodes[v][i]]=true;
+               // cout<<"visited"<<endl;
+            }
+            if(c[nodes[v][i]]>(c[v]+1))
+                {
+                    c[nodes[v][i]]=c[v]+1;
+                    //cout<<c[nodes[v][i]]<<endl;
+                }
+        }
+    }
+
+    return c[n*m];
+
+}
+
+
+int main()
+{
+
+    int x,y,s,e;
+
+    cin>>m>>n;
+    //m is number of nodes
+    //n is number of edges
+
+    for(int i=1;i<=n;i++)
+    {
+        cin>>x>>y;
+        nodes[x].push_back(y);
+    }
+
+    cin>>s>>e; //starting position or from where to start and ending position
+
+    BFS(s);
+
+    cout<<c[e]<<endl;
+}
